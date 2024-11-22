752c929e6e79135c0f178dcf06dc0e2bc940231d
suhejb-v <sv70590@ubt-uni.net>
2024-11-22 11:18:33 +0200

    Initial commit

---
+++ Detyra1.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').onclick = function () {
+    var emri = prompt('Shkruani emrin tuaj:');
+    var qyteti = prompt('Shkruani qytetin tuaj:');
+    alert('Pershendetje ' + emri + '! Ju jetoni ne ' + qyteti + '.');
+};
